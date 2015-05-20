bd7defbb86ed9cf713de0cb29b16dfe91c343305
gsvr <george@westerncapelabs.com>
2015-05-20 14:55:29 +0200

    migrations

---
+++ django_refugeerights/metricsdownloader/migrations/0001_initial.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='Metric',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('lookup_name', models.CharField(max_length=255)),
+                ('display_name', models.CharField(max_length=25)),
+                ('created_at', models.DateTimeField(auto_now_add=True)),
+                ('updated_at', models.DateTimeField(auto_now=True)),
+            ],
+        ),
+        migrations.CreateModel(
+            name='MetricRecord',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('date_recorded', models.DateTimeField()),
+                ('value', models.IntegerField()),
+                ('created_at', models.DateTimeField(auto_now_add=True)),
+                ('updated_at', models.DateTimeField(auto_now=True)),
+                ('metric', models.ForeignKey(related_name='metric', to='metricsdownloader.Metric')),
+            ],
+        ),
+    ]
