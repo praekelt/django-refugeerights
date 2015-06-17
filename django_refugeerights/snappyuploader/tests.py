2a265190413f7709890dc4584a7eb70246198c8f
Mike Jones <mike@westerncapelabs.com>
2015-06-17 21:35:20 +0200

    Add test for not duplicating existing

--- django_refugeerights/snappyuploader/tests.py
+++ django_refugeerights/snappyuploader/tests.py
@@ -54,3 +54,27 @@ class SnappyFaqSyncTest(TestCase):
         self.assertEqual(sync_result.get(),
                          "FAQs synced. Created FAQs: Refugee FAQ 2222\n"
                          "Refugee FAQ 2223\n")
+
+    @responses.activate
+    def test_sync_faqs_no_new(self):
+        snappy_response = [{"id": 1,
+                            "account_id": 12345,
+                            "title": "Test FAQ",
+                            "url": "already-in-database",
+                            "custom_theme": "",
+                            "culture": "en",
+                            "navigation": "{Nav}",
+                            "created_at": "2013-11-16 19:14:17",
+                            "updated_at": "2013-11-19 07:46:59",
+                            "order": 0}]
+
+        responses.add(responses.GET,
+                      "https://app.besnappy.com/api/v1/account/12345/faqs",
+                      json.dumps(snappy_response),
+                      status=200, content_type='application/json')
+
+        sync_result = sync_faqs.delay()
+
+        faqs = SnappyFaq.objects.all()
+        self.assertEqual(len(faqs), 1)
+        self.assertEqual(sync_result.get(), "FAQs synced. Created FAQs: ")
