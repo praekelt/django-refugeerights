5894994aaab5c0b350f6cc4fd85746f3a0e09a98
Simon Cross <hodgestar@gmail.com>
2016-04-11 12:55:19 +0200

    Refactor to use csv.writer (so that rows are escaped properly).

--- faqs/csv_parser.py
+++ faqs/csv_parser.py
@@ -18,7 +18,7 @@ class CsvContentParser(object):
         self.delimiter = delimiter
 
     def parse(self):
-        topic_map = {
+        topics = {
             "__current__": None
         }
 
@@ -26,56 +26,48 @@ class CsvContentParser(object):
             reader = csv.reader(csvfile, delimiter=self.delimiter,
                                 quotechar='"')
             for row in reader:
-                self.handle_row(row, topic_map)
+                self.handle_row(row, topics)
 
-        return self
+        del topics["__current__"]
+        return topics
 
-    def handle_row(self, row, topic_map):
+    def handle_row(self, row, topics):
         if len(row[0].strip()) > 0:
             new_topic = row[2].strip()
-            topic_map[new_topic] = []
-            topic_map["__current__"] = new_topic
+            topics[new_topic] = []
+            topics["__current__"] = new_topic
 
         if row[4] == 'Back' or row[7] == 'Back' or len(row[7].strip()) == 0:
             return
 
-        current_topic = topic_map["__current__"]
-        assert current_topic is not None
+        current_topic = topics["__current__"]
         entry = RowEntry(self.locale, row[6], row[10], row[14])
-        topic_map[current_topic].append(entry)
+        topics[current_topic].append(entry)
         return
 
-    def save(self, destination_file):
+    def save(self, destination_file, topics):
         with open(destination_file, 'w') as output_file:
-            output_file.write('locale,topic,question,answer\n')
-            for topic in self.topic_map:
-                for row in self.topic_map[topic]:
-                    output_file.write("{0}\n".format(row.__str__()))
+            writer = csv.writer(output_file)
+            writer.writerow(['locale', 'topic', 'question', 'answer'])
+            for topic in sorted(topics.keys()):
+                for row in topics[topic]:
+                    writer.writerow([
+                        row.locale, row.topic, row.question, row.answer])
 
 
 class RowEntry(object):
 
     def __init__(self, locale, topic, question, answer):
-        self.locale = self.escape_string(locale)
-
-        self.topic = self.escape_string(topic)
-        self.question = self.escape_string(question)
-        self.answer = self.escape_string(answer)
-
-    def __str__(self):
-        return "{0},{1},{2},{3}".format(self.locale, self.topic, self.question,
-                                        self.answer)
-
-    def escape_string(self, text, delimiter=',', force=False):
-        if (delimiter in text) or force:
-            return '"{0}"'.format(text)
-        else:
-            return text
+        self.locale = locale
+        self.topic = topic
+        self.question = question
+        self.answer = answer
 
 
 def main():
-    CsvContentParser('migrant-content.csv', 'so', ',').parse().save(
-        'output-migrant-content.csv')
+    csv_parser = CsvContentParser('migrant-content.csv', 'so', ',')
+    topics = csv_parser.parse()
+    csv_parser.save('output-migrant-content.csv', topics)
 
 
 if __name__ == "__main__":
