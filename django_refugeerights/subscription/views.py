540cd4b99efb66b4ef7e79caf629140cdc28d239
TrevorOctober <trevor@praekeltconsulting.com>
2016-04-20 11:49:30 +0200

    trying to allow user to switch status

--- django_refugeerights/subscription/views.py
+++ django_refugeerights/subscription/views.py
@@ -1,7 +1,11 @@
 from subscription.models import Subscription
+from rest_framework.views import APIView
 from rest_framework import viewsets
 from rest_framework.permissions import IsAuthenticated
+from rest_framework import permissions
 from subscription.serializers import SubscriptionSerializer
+from rest_framework.exceptions import ValidationError
+from django.http import HttpResponse
 
 
 class SubscriptionViewSet(viewsets.ModelViewSet):
@@ -13,3 +17,27 @@ class SubscriptionViewSet(viewsets.ModelViewSet):
     queryset = Subscription.objects.all()
     serializer_class = SubscriptionSerializer
     filter_fields = ('contact_key', 'to_addr', 'active', )
+
+
+class SwitchSubscriptionView(APIView):
+    permission_classes = (permissions.IsAuthenticated,)
+
+    def post(self, request, format=None):
+        data = request.data
+        try:
+            contact_key = data['contact_key']
+        except KeyError:
+            raise ValidationError("A contact 'key' parameter is required.")
+        try:
+            messageset_id = data['messageset_id']
+        except KeyError:
+            raise ValidationError("A messageset 'id' parameter is required.")
+        return HttpResponse(status=200)
+        try:
+            messageset = MessageSet.objects.filter(pk=messageset_id).exists()
+        except KeyError:
+            raise ValidationError("A messageset 'id' doesn't exist")
+        try:
+            subscription = Subscription.objects.filter(pk=contact_key).update(active=False)
+        except KeyError:
+            raise ValidationError("")
