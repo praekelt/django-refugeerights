2811025a563add43a2ea6616dd85765654775d30
Simon Cross <hodgestar@gmail.com>
2016-04-21 18:58:54 +0200

    Drastically reworkd CSV parser to match reality.

--- faqs/csv_parser.py
+++ faqs/csv_parser.py
@@ -1,59 +1,75 @@
 import csv
+import os
+import sys
 import unicodedata
 
 
-class CsvContentParser(object):
+class CsvConverter(object):
 
-    def __init__(self, filename, locale, delimiter):
-        if len(filename) < 1:
-            raise RuntimeError("Filename cannot be blank")
-
-        if len(locale) != 2:
-            raise RuntimeError("Locale must have a length of 2")
-
-        if len(delimiter) != 1:
-            raise RuntimeError("delimiter must have a length of 1")
-
-        self.filename = filename
-        self.locale = locale
+    def __init__(self, delimiter=',', quotechar='"'):
         self.delimiter = delimiter
+        self.quotechar = quotechar
 
-    def parse(self):
-        topics = {
-            "__current__": None
+    def parse(self, csvfile, locale):
+        if len(locale) != 2:
+            raise ValueError("Locale must have a length of 2")
+        locale = self._decode(locale)
+
+        faqs = {
+            "__current_faq__": None,
+            "__current_topic__": None,
         }
 
-        with open(self.filename, 'rb') as csvfile:
-            reader = csv.reader(csvfile, delimiter=self.delimiter,
-                                quotechar='"')
-            for row in reader:
-                self.handle_row(row, topics)
+        reader = csv.reader(
+            csvfile, delimiter=self.delimiter, quotechar=self.quotechar)
+        for row in reader:
+            self._handle_row(row, faqs, locale)
 
-        del topics["__current__"]
-        return topics
+        del faqs["__current_faq__"]
+        del faqs["__current_topic__"]
+        return faqs
 
-    def handle_row(self, row, topics):
-        if len(row[0].strip()) > 0:
-            new_topic = row[2].strip()
-            topics[new_topic] = []
-            topics["__current__"] = new_topic
+    def _decode(self, s):
+        return s.decode("utf-8")
+
+    def _handle_row(self, row, faqs, locale):
+        current_faq = faqs.get("__current_faq__")
+        current_topic = faqs.get("__current_topic__")
+
+        new_faq = row[0].strip().replace("/", "")
+        if new_faq and new_faq != current_faq:
+            current_faq = faqs["__current_faq__"] = new_faq
+            current_topic = None
+            faqs[current_faq] = {}
+        faq = faqs[current_faq]
+
+        new_topic = row[2].strip()
+        if new_topic and new_topic != current_topic:
+            current_topic = faqs["__current_topic__"] = new_topic
+            faq[current_topic] = []
+            return
+        topic = faq[current_topic]
 
         if row[4] == 'Back' or row[7] == 'Back' or len(row[7].strip()) == 0:
             return
 
-        current_topic = topics["__current__"]
-        entry = RowEntry(self.locale, row[6], row[10], row[14])
-        topics[current_topic].append(entry)
+        entry = RowEntry(
+            locale, self._decode(row[6]), self._decode(row[10]),
+            self._decode(row[15]))
+        topic.append(entry)
         return
 
-    def save(self, destination_file, topics):
-        with open(destination_file, 'w') as output_file:
-            writer = csv.writer(output_file)
-            writer.writerow(['locale', 'topic', 'question', 'answer'])
-            for topic in sorted(topics.keys()):
-                for row in topics[topic]:
-                    writer.writerow([
-                        row.locale, row.topic, row.question, row.answer])
+    def save(self, refugee_type, locale, faqs):
+        for faq in sorted(faqs.keys()):
+            output_file = os.path.join(refugee_type, locale, "%s.csv" % faq)
+            with open(output_file, "wb") as f:
+                writer = csv.writer(f)
+                writer.writerow(['locale', 'topic', 'question', 'answer'])
+                topics = faqs[faq]
+                for topic in sorted(topics.keys()):
+                    for row in topics[topic]:
+                        writer.writerow([
+                            row.locale, row.topic, row.question, row.answer])
 
 
 class RowEntry(object):
@@ -73,10 +89,17 @@ def closest_ascii(s):
 
 
 def main():
-    csv_parser = CsvContentParser('migrant-content.csv', 'so', ',')
-    topics = csv_parser.parse()
-    csv_parser.save('output-migrant-content.csv', topics)
+    if len(sys.argv) != 3:
+        print(
+            "Usage: %s <refugee-or-migrant> <locale>" % sys.argv[0])
+        return 1
+    [refugee_type, locale] = sys.argv[1:]
+    converter = CsvConverter()
+    input_file = os.path.join("src", refugee_type, "%s.csv" % locale)
+    with open(input_file, "rb") as f:
+        faqs = converter.parse(f, locale)
+    converter.save(refugee_type, locale, faqs)
 
 
 if __name__ == "__main__":
-    main()
+    sys.exit(main())
